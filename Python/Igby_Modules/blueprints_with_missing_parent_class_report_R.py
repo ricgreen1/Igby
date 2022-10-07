0e2409eb3ddc7c101ff12b69b96d09c005fe9a23
Richard Greenspan <ricgreen1@hotmail.com>
2022-10-07 11:25:52 -0400

    Finds blueprints with missing parent class
    This module will identify all blueprints that have a missing parent class.

---
+++ Python/Igby_Modules/blueprints_with_missing_parent_class_report_R.py
@@ -0,0 +1,48 @@
+#blueprints_with_missing_parent_class_report_R.py for Igby UE Project Automator
+# Developed by Richard Greenspan | rg.igby@gmail.com
+# Licensed under the MIT license. See LICENSE file in the project root for details.
+
+import os, unreal, igby_lib, ue_asset_lib
+
+def run(settings):
+    
+    #get setting
+    paths_to_include = settings['PATHS_TO_INCLUDE']
+    paths_to_ignore = settings['PATHS_TO_IGNORE']
+
+    #setup logger
+    logger = igby_lib.logger()
+    logger.prefix = "    "
+
+    logger.log_ue("Identifying blueprints with parent class that does not exist.")
+    logger.log_ue("")
+    logger.log_ue("This is usuall due to a parent class that has not been submitted or deleted.", "info_clr")
+    logger.log_ue("These blueprints should be remapped to an existing class or removed to improve project integrity.", "info_clr")
+
+    total_asset_count = 0
+
+    filtered_assets = ue_asset_lib.get_assets(paths_to_include, paths_to_ignore, True)
+    blueprint_assets = ue_asset_lib.keep_assets_of_class(filtered_assets, "Blueprint")
+
+    blueprints_with_missing_parent_class = set()
+
+    for blueprint in blueprint_assets:
+
+        total_asset_count+=1
+
+        bp_class_object_path = '{}_C'.format(blueprint.object_path)
+        bp_gen_object = unreal.load_asset(bp_class_object_path)
+        blueprint_class_default = unreal.get_default_object(bp_gen_object)
+
+        if isinstance(blueprint_class_default, type(None)):
+            blueprints_with_missing_parent_class.add(blueprint.package_name)
+
+    logger.log_ue("")
+    logger.log_ue("Scanned {} blueprints.\n".format(total_asset_count))
+    logger.log_ue("The following is a list of blueprints that have a missing parent class:\n")
+
+    for blueprint in blueprints_with_missing_parent_class:
+
+        logger.log_ue(blueprint)
+
+    return True
